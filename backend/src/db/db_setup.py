0cf4679d6bc650904e1d1f9553a8e507e140bfb4
felippodev <coelho.luizfelippo@gmail.com>
2023-10-23 02:38:25 -0300

    Chore: udpating requirements.txt and .env.example

--- backend/src/db/db_setup.py
+++ backend/src/db/db_setup.py
@@ -1,15 +1,21 @@
 from typing import Annotated
 from fastapi import Depends
-from decouple import config
+from starlette.config import Config
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
 
-SQLALCHEMY_DATABASE_URL = config("SQLALCHEMY_DATABASE_URL")
+config = Config(".env")
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+DB_USER = config("DB_USER")
+HOST = config("HOST")
+PORT = config("PORT")
+DB_PASSWORD = config("DB_PASSWORD")
+DB_NAME = config("DB_NAME")
+
+SQLALCHEMY_DATABASE_URL = F'postgresql://{DB_USER}:{DB_PASSWORD}@{HOST}:{PORT}/{DB_NAME}'
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
